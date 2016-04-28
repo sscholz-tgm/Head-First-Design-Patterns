82505c8d86de4d5abc89f953c8331eb2bb98fe0a
sscholz-tgm <sscholz@student.tgm.ac.at>
2016-04-28 10:43:02 +0200

    added a comment for the AbstractFactory class

--- src/headfirst/designpatterns/combining/observer/AbstractDuckFactory.java
+++ src/headfirst/designpatterns/combining/observer/AbstractDuckFactory.java
@@ -1,5 +1,13 @@
 package headfirst.designpatterns.combining.observer;
 
+/**
+ * 
+ * (Commentator sscholz-tgm)
+ * The Abstract Factory of the Factory Pattern
+ * is then instanciated in the Ducksimulator class
+ * object then not of type concrete class but interface
+ *
+ */
 public abstract class AbstractDuckFactory {
  
 	public abstract Quackable createMallardDuck();
